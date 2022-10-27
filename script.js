a7cf4037115148e1f0850ae5485321ec0e9b0924
Jackson Grimm <92557509+Bdogy@users.noreply.github.com>
2022-10-27 17:43:03 -0600

    added new function to script.js, promted for length of password and commit messages

--- script.js
+++ script.js
@@ -1,5 +1,87 @@
 // Assignment code here
 
+function generatePassword() {
+  // declaring all character list
+  var lowerCaseArr = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+
+  var upperCaseArr = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+
+  var numbersArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+  var specialArr = ["!", "@", "#", "$", "*", "%"];
+
+  //User Length input
+  var userLength = prompt("Enter Password Length! 8-128 characters only");
+  //makes Data type number instead of str
+  userLength = Number(userLength);
+
+  //confirms for charas
+  var upperCase = confirm("Would you like Uppercase? ");
+  var lowerCase = confirm("Would you like Lowercase? ");
+  var numbers = confirm("Would you like numbers? ");
+  var specialChara = confirm("Would you like specialChara? ");
+
+  var wantedChara = [];
+
+  //check booleans confirms
+  if (upperCase) {
+    wantedChara = wantedChara.concat(upperCaseArr);
+  }
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -10,7 +92,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
